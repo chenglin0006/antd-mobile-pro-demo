d0440110a305a10d67a6440e06ec97279939f035
lin.cheng@b-and-qchina.com <lin.cheng@b-and-qchina.com>
2023-07-17 16:36:52 +0800

    feat(mobile): update

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd';
 import { connect } from 'react-redux';
-import pathToRegexp from 'path-to-regexp';
 import './BasicLayout.less';
 import Contents from './Contents';
 
@@ -12,72 +11,32 @@ class BasicLayout extends React.PureComponent {
 
   static propTypes = {
     getCurrentUser: PropTypes.func,
-    logout: PropTypes.func,
-    isInQiankun: PropTypes.bool,
-    isShop: PropTypes.bool,
   };
 
   static defaultProps = {
     getCurrentUser: () => {},
-    isShop: false,
   };
 
   constructor(props) {
     super(props);
-    this.state = {
-      collapsed: false, // 当前侧边栏收起状态
-    };
+    this.state = {};
   }
 
   componentDidMount() {
     this.props.getCurrentUser();
   }
 
-  onMenuClick = ({ key }) => {
-    const { logout } = this.props;
-
-    if (key === 'logout') {
-      logout({ shouldRequest: true, redirectUrl: `${location.origin}/` });
-    }
-  };
-
-  /**
-   * 设置菜单收缩状态
-   */
-  setMenuCollapsed = (iscollapsed) => {
-    const collapsed = iscollapsed || !this.state.collapsed;
-    this.setState({ collapsed });
-  };
-
-  getRouterAuthority = (pathname, routeData) => {
-    let routeAuthority;
-    // 递归遍历路由获取authority，子路由若没设置authority，将继承父路由authority
-    const getAuthority = (key, routes) => {
-      routes.forEach((route) => {
-        if (route.path && pathToRegexp(route.path, [], { end: false }).test(key) && route.authority) {
-          routeAuthority = route.authority;
-        }
-        if (route.routes) {
-          routeAuthority = getAuthority(key, route.routes);
-        }
-      });
-      return routeAuthority;
-    };
-    return getAuthority(pathname, routeData);
-  };
-
   render() {
-    const { isInQiankun, isShop } = this.props;
     return (
-      <BrowserRouter basename={isInQiankun ? '/todocenter-web' : '/'}>
+      <BrowserRouter>
         <Switch>
           <Route
             render={() => {
               return (
-                <Layout className={`todocenter-container ${isInQiankun ? 'in-qk' : 'out-qk'}`}>
+                <Layout className="todocenter-container">
                   <Layout className="right-layout">
                     <Layout className="ant-layout-ie9">
-                      <Contents isInQiankun={isInQiankun} isShop={isShop} />
+                      <Contents />
                     </Layout>
                   </Layout>
                 </Layout>
